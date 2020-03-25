c2bb00130b1b65355a36254a46ad7a3a70b8aaf6
powellstreet <codestates.jc@gmail.com>
2020-03-26 00:01:33 +0900

    Summary : Opentutorials Cookies Exercise

--- OT_Cookies/cookies.js
+++ OT_Cookies/cookies.js
@@ -13,7 +13,7 @@ http.createServer(function(request, response){
         'Set-Cookie': [ // cookie가 복수일 경우 배열로 넣기
             'yummy_cookie=choco', 
             'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*60*24}`, // 브라우저가 종료되어도 남아있도록 하는 쿠키. 언제까지 남아있을지를 초 단위로 세팅.
+            `Permanent=cookies; Max-Age=${60*60*60*24}`, // 브라우저가 종료되어도 남아있도록 하는 쿠키. 언제까지 남아있을지를 초 단위로 세팅. 0으로 세팅하면 삭제됨.
             'Secure=Secure; Secure', // http가 아닌 https 일때만 보이는 쿠키
             'HttpOnly=HttpOnly; HttpOnly', // javascript 에서는 보이지 않는 쿠키. 크롬 콘솔(document.cookie)에는 보이지 않으나 실재로 존재함.
             'Path=Path; Path=/cookie', // 특정 디렉토리에서만 쿠키가 활성되게 하고 싶을 때 사용.
